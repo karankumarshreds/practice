7d6fdedadf676f9f4274c8c4ecffb13ca4d722ab
karankumarshreds <naileditnerd@gmail.com>
2022-01-18 23:39:36 +0530

    update

--- pages/heros/index.tsx
+++ pages/heros/index.tsx
@@ -10,10 +10,16 @@ interface Node {
 }
 
 export default function () {
+  const [counter, setCounter] = React.useState(0);
   const { data, isLoading } = useQuery(Keys.fetch_heroes, () => {
     return axios.get('http://localhost:4000/superheroes') as Promise<UseQueryResult<Node[]>>;
   });
-  console.log(data);
   if (isLoading) return <h1>Loading...</h1>;
-  return <ul>{data && data.data && data.data.map((each) => <li>{each.name}</li>)}</ul>;
+
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Count</button>
+      <ul>{data && data.data && data.data.map((each) => <li key={each.id}>{each.name}</li>)}</ul>
+    </div>
+  );
 }
